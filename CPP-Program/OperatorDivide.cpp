deed33fd80305805bf857d0a45053d6992547563
DEvackemann <anshikadubey8591@gmail.com>
2024-03-14 23:15:04 +0530

    daily commit

---
+++ CPP-Program/OperatorDivide.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int a= 2.9/5;
+  cout << a<< endl;
+    cout<< "double:"<< 2.34/3 <<endl;
+    cout<< "float:"<< 6.0 /3 <<endl;
+
+//RELATIONAL OPERATOR 
+//  =, < , >, <=, >=, !=
+int num1 = 3; 
+int num2= 4;
+bool check1 = (num1==num2);
+cout<< check1 <<endl;
+bool check2 = (num1!=num2);
+cout<< check2 <<endl;
+bool check3 = (num1>=num2);
+cout<< check3 <<endl;
+bool check4 = (num1<=num2);
+cout<< check4 <<endl;
+bool check5 = (num1<num2);
+cout<< check5 <<endl;
+bool check6 = (num1>num2);
+cout<< check6 <<endl;
+
+
+
+}
---
+++ CPP-Program/typeCasting.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 'a';
+    cout << a << endl;
+    char ch = 98;
+    cout << ch << endl;
+    char ch1= 123456;  // overflow @
+    cout<< ch1<< endl;
+    int d= -5;
+    cout<< d<< endl;
+    // for printing only positive integer i.e  0 to 2^32 - 1  use UNSIGNED 
+    unsigned int num = 2133;
+    cout<< num << endl;
+    unsigned int num2= -3243454;  // give huge value 
+    cout << num2 << endl;
+
+
+}
+
+
